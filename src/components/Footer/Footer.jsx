2d68dc330d02114717e0629599c7f0caeea67420
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-09-05 22:47:15 -0300

    adding LinkIcons to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import { LinkIcon } from "../index";
+
 import "./Footer.scss";
 
 const Footer = () => {
@@ -5,11 +7,13 @@ const Footer = () => {
     <footer className="footer-bcontainer">
       <div className="footer-container">
         <div className="fc-links">
-
+          <LinkIcon icon="logo-github" link="https://github.com/NahuelGenchi"/>
+          <LinkIcon icon="logo-linkedin" link="https://www.linkedin.com/in/nahuel-genchi"/>
+          <LinkIcon icon="mail-outline" link="mailto:nahuelgenchi@gmail.com"/>
         </div>
         <div className="fc-info">
           <div>© 2022 Nahuel Genchi</div>
-          <div>Data received by <a href="https://randomuser.me/">randomuser.me API</a></div>
+          <div>Data received by <a href="https://randomuser.me/" target="_blank" rel="noreferrer">randomuser.me</a> API</div>
         </div>
       </div>
     </footer>
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as UserContainer } from "./UserContainer/UserContainer";
 export { default as User } from "./User/User";
+export { default as LinkIcon } from "./LinkIcon/LinkIcon";
 export { default as Footer } from "./Footer/Footer";
